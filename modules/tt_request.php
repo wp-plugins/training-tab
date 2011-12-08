a9744250c61151b3071e0b1e1e96d3d550c1affe
TechStudioWP <TechStudioWP@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-08 00:06:09 +0000

    ver 1.1.4
    git-svn-id: https://plugins.svn.wordpress.org/training-tab/tags/1.1.4@472247 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ modules/tt_request.php
@@ -0,0 +1,109 @@
+<?php
+		global $wpdb;
+		$settings_table = $wpdb->prefix . "tt_settings";		
+
+		
+		if (isset($_POST['send_it'])){
+			foreach ($current_settings as $setting) { };
+			$toemail = $setting->email_to;
+			if (empty($toemail)){
+				$toemail = "training@techstudio.co";
+			}
+
+			$name = $_POST['name'];
+			$company = $_POST['company'];
+			$email = $_POST['email'];
+			$phone = $_POST['phone'];
+			$area = $_POST['area_of_training'];
+			$method = $_POST['type_of_training'];
+			$due_by = $_POST['how_soon'];
+			$payment = $_POST['payment_method'];
+			$how_to_add = $_POST['how_to_add'];
+			$description = $_POST['description'];
+			
+			$to      = $toemail;
+			$subject = 'techstudio.co >> Training Tab >> training request '.$_SERVER['HTTP_HOST'];
+			$headers = 'From: '.$email. "\r\n" .
+			'Reply-To: '.$email. "\r\n" .
+			'X-Mailer: PHP/' . phpversion();
+
+			$message = 
+			'New Training Request'. "\r\n" .
+			'Name: '.$name. "\r\n" .
+			'Company Name: '.$company. "\r\n" .
+			'E-mail: '.$email. "\r\n" .
+			'Phone: '.$phone. "\r\n" .
+			'Training required in '.$area. "\r\n" .
+			'Training Method '.$method. "\r\n" .
+			'Training is due '.$due_by. "\r\n" .
+			'Method of Payment '.$payment. "\r\n" .
+			'How to add the Training Video/Materials '.$how_to_add. "\r\n" .
+			'Description of training'."\r\n".$description. "\r\n" .
+			'This is an automated Alert - PLEASE DO NOT REPLY'
+			;
+			
+		mail($to, $subject, $message, $headers);
+		echo '<div class="manage-response"><p>Your Training Request has been sent.</p></div>';
+			
+		}
+		
+?>
+<div class="training-tab-wrapper wrap">
+<?php tt_header(); ?>
+
+	<div class="section">
+		<h4>Request Training</h4>
+		<form id="request_form" method="POST">
+			<h5>Name:</h5>
+			<input type="text" name="name" value=""><br />
+			<h5>Company Name:</h5>
+			<input type="text" name="company" value=""><br />
+			<h5>E-mail:</h5>
+			<input type="text" name="email" value=""><br />
+			<h5>Phone:</h5>
+			<input type="text" name="phone" value=""><br />
+			<p>This information is required in order to properly process your order. Your phone number is optional, but will expedite your request.</p>
+			<br />
+			<h5>If possible, define the area in which you require training.</h5><br />
+			<select name="area_of_training">
+				<option value="na">--</option>
+				<option value="getting-started">Getting Started: Installation, Setup, Etc.</option>
+				<option value="basics">Working With WordPress: Basics, Posts, Pages, Images, Etc.</option>
+				<option value="design">Design and Layout</option>
+				<option value="advanced">Advanced Topics</option>
+				<option value="troubleshooting">Troubleshooting</option>
+			</select><br /><br />
+			<h5>What type of training best suits your request?</h5><br/>
+			<select name="type_of_training">
+				<option value="video">Subject-targeted Training Video</option>
+				<option value="telephone">Custom video created for your specific question</option>
+				<option value="desktop">One-on-one training with video and audio recording for review</option>	
+			</select><br/><br />
+			<h5>How urgently is this training required?</h5><br/>	
+			<select name="how_soon">	
+				<option value="whenever">I am in no rush</option>
+				<option value="soon">Soon, I'd like to learn this so I can keep working</option>
+				<option value="now">It's an emergency, I'm in a bind until I get this figured out</option>
+			</select><br /><br />
+			<h5>Payment Method</h5><br/>	
+			<select name="payment_method">
+				<option value="paypal">PayPal Invoice</option>
+				<option value="google">Google Checkout</option>
+				<option value="techstudio">Bill Me (requires account)</option>
+			</select><br /><br />
+			<h5>How would you like your training delivered?</h5><br />	
+			<select name="how_to_add">
+				<option value="na">--</option>
+				<option value="email">E-mail it and I'll add it to Training</option>
+				<option value="upload">Add it to Training Tab for me</option>
+			</select><br /><br />
+			<h5>Describe the training you would like to request.</h5><br />
+			<textarea name="description" rows="10" cols="30"></textarea><br /><br />
+			<input type="submit" name="send_it" value="Send Request">
+		</form>
+	</div>
+
+</div>
+<?php
+
+?>
---
+++ tags/1.1.2/tt_main.php
@@ -0,0 +1,48 @@
+<?php ?>
+
+<div class="training-tab-wrapper wrap">
+<?php tt_header(); ?>
+	<h3>Your Training Items</h3>
+	<div class="section table-of-contents">
+		<h4>Table of Contents</h4>
+		<?php
+			global $wpdb;
+			$table_name = $wpdb->prefix . "tt_training";
+			$current_training = $wpdb->get_results("SELECT * FROM $table_name");
+		?>
+		<ul>
+			<li>
+				<a href="#">How-To Videos</a>
+				<ul>
+					<?php foreach ($current_training as $contents){ 
+						echo '<li><a href="#Anchor'.$contents->id.'">'.$contents->training_title.'</a></li>';
+					}
+					?>					
+				</ul>
+			</li>
+		</ul>
+	</div>
+	
+	<h3>How-To Videos</h3>
+<?php
+	//testing new code for importing from DB
+			foreach ($current_training as $training){		
+?>
+	<div class="section" id="record-id-<?php echo $training->id ?>">
+		<h4><?php echo $training->training_title ?></h4>
+		<?php echo stripslashes($training->training_content) ?>
+		<h5>Screencast Video: <?php echo $training->training_title ?></h5>
+		<div class="screencast-video">
+<?php echo stripslashes($training->training_embed) ?>
+		</div>
+		<div class="show-hide-wrapper"><div class="show-hide-acc">Show Video</div></div>
+	</div>
+		<a name="Anchor<?php echo $training->id ?>"></a>
+<?php
+}; //end foreach loop that pulls the info
+//end testing
+?>	
+
+</div>
+
+<?php ?>
